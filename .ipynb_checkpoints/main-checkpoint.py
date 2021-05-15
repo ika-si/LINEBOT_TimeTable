a062f5414af0be865034203638cb038f4580729c
ika-si <ikachan.0630@gmail.com>
2021-05-15 21:52:11 +0900

    文面変更

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -16,17 +16,17 @@ def main():
     
     USER_ID = info['USER_ID']
     if (weekday == 6):
-        messages = TextSendMessage(text="1.\n2.メディア分析法(10:40~\n3.\n4.教育相談の理論と方法\n5.教育相談の理論と方法\n6.情報科指導法")
+        messages = TextSendMessage(text="今日は6限まである!死ぬな!\n1.\n2.メディア分析法(10:40~\n3.\n4.教育相談の理論と方法\n5.教育相談の理論と方法\n6.情報科指導法")
     elif (weekday == 0):
-        messages = TextSendMessage(text="1.データベース入門\n2.数学科指導法\n3.インターラクティブシステム\n4.\n5.センサー入門\n")
+        messages = TextSendMessage(text="1限[ビデオオン]あるぞ!起きて！\n1.データベース入門\n2.数学科指導法\n3.インターラクティブシステム\n4.\n5.センサー入門\n")
     elif (weekday == 1):
-        messages = TextSendMessage(text="1.\n2.自然言語処理\n3.３年プロジェクト\n4.\n5.\n")
+        messages = TextSendMessage(text="今日は大学に行く日だ!\n1.\n2.自然言語処理\n3.３年プロジェクト\n4.\n5.\n")
     elif (weekday == 2):
-        messages = TextSendMessage(text="1.情報科学英語a\n2.数理モデル\n3.数理モデル\n4.\n5.情報数学a\n")
+        messages = TextSendMessage(text="無の木曜日!\n1.情報科学英語a\n2.数理モデル\n3.数理モデル\n4.\n5.情報数学a\n")
     elif (weekday == 3):
-        messages = TextSendMessage(text="1.\n2.\n3.情報科学c\n4.ソフトウェア開発法\n5.ソフトウェア開発法\n")
+        messages = TextSendMessage(text="午前休でも起きて家事するぞ\n1.\n2.\n3.情報科学c\n4.ソフトウェア開発法\n5.ソフトウェア開発法\n")
     else:
-        messages = TextSendMessage(text="休日だけど、起きて!今日も一日頑張ろう！")
+        messages = TextSendMessage(text="休日だけど、起きて!課題溜まってるでしょ！やれー！！！")
         
     line_bot_api.push_message(USER_ID, messages=messages)
 
